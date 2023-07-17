b49b01a89f2f04fb4fae2e7d091ee85c7353fa58
KristiSeraj <serajkristi@gmail.com>
2023-07-17 12:26:07 +0200

    private password

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,18 @@
+class Password {
+    String _password;
+
+    Password({String password = ''}) : _password = password;
+    
+
+    bool isValid() {
+        if ((_password.length > 8 && _password.length < 16) && _password.contains(RegExp(r'^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])'))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    String toString() {
+        return 'Your Password is: $_password';
+    }
+}
